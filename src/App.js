472ff000b0c2f9d97b480ba777898de5add77d1a
BayanL-Daleel <bayan.dl96@gmail.com>
2025-02-18 19:39:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
 
 function App() {
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
+
+  async function startGame(e) {
+    e.preventDefault();
+    try {
+      const response = await fetch(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
+      if (!response.ok) {
+        throw new Error("Could not fetch data from API");
+      }
+      const data = await response.json();
+      const dataSlice =await getDataSlice(data);
+      const emojisArray= await getEmojisArray(dataSlice)
+      setEmojisData(emojisArray);
+
+    } catch (err) {
+      console.error(err);
+    }
+    setIsGameOn(true);
+  }
+  function getDataSlice(data) {
+    const randomIndicies = getRandomIndices(data);
+    const dataSlice = randomIndicies.map((index) => data[index]);
+    return dataSlice;
+  }
+
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+    return randomIndicesArray;
+  }
+  async function getEmojisArray(data){
+    const pairedEmojisArray=[...data,...data]
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      const temp = pairedEmojisArray[i]
+      pairedEmojisArray[i] = pairedEmojisArray[j]
+      pairedEmojisArray[j] = temp
+  }
+    return pairedEmojisArray
+  }
+
+  function turnCard(index,name) {
+    console.log("Memory card clicked");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} data={emojisData} />}
+    </main>
   );
 }
 
